0363671e2769d167dbf4047ff4f7805bae2ed33b
James Kelly <jk_jameskelly@outlook.com>
2017-07-25 17:35:07 -0400

    create user

--- src/schema/index.js
+++ src/schema/index.js
@@ -11,11 +11,29 @@ const typeDefs = `
 
   type Query {
     allLinks: [Link!]!
+    allUsers: [User!]!
   }
 
   type Mutation {
     createLink(url: String!, description: String!): Link
+    createUser(name: String!, authProvider: AuthProviderSignupData!): User
   }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String
+  }
+
+  input AuthProviderSignupData {
+    email: AUTH_PROVIDER_EMAIL
+  }
+
+  input AUTH_PROVIDER_EMAIL {
+    email: String!
+    password: String!
+  }
+
 `;
 
 module.exports = makeExecutableSchema({
